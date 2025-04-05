8bcbdc80cf2c12dbe745ce79f3f52559e3a1493f
Bj-LAI <bjornlai13@gmail.com>
2025-04-05 22:09:19 +0200

    Ajout du projet AndroidStudio

---
+++ app/src/main/java/com/iarpg/app/data/GameBoard.java
@@ -0,0 +1,94 @@
+package com.iarpg.app.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameBoard {
+    private final List<List<Room>> grid;
+    private final int height;
+    private final int width;
+    private Position currentPosition;
+    private final Position startPosition;
+    private final Position exitPosition;
+
+    public GameBoard(int height, int width) {
+        this.height = height;
+        this.width = width;
+
+        startPosition = generateRandomStartPosition();
+        exitPosition = generateRandomExitPosition();
+        currentPosition = new Position(startPosition.getX(), startPosition.getY());
+
+        grid = this.generateGrid();
+    }
+
+    public Position getStartPosition() {
+        return startPosition;
+    }
+
+    public Position getExitPosition() {
+        return exitPosition;
+    }
+
+    public Position getCurrentPosition() {
+        return currentPosition;
+    }
+
+    private List<List<Room>> generateGrid() {
+        List<List<Room>> grid = new ArrayList<>();
+
+        for (int y = 0; y < height; y++) {
+            grid.add(new ArrayList<>());
+        }
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (new Position(x, y).equals(exitPosition)) {
+                    grid.get(y).add(new Room("", "")); // todo : ajouter Room finale
+                }
+                else {
+                    grid.get(y).add(new Room("", "")); // todo : Ajouter description et scenario (générés par IA)
+                }
+
+            }
+        }
+        return grid;
+    }
+
+    public void movePlayer(String direction) {
+        switch (direction) {
+            case "left":
+                currentPosition.setX(currentPosition.getX() - 1);
+                break;
+
+            case "right":
+                currentPosition.setX(currentPosition.getX() + 1);
+                break;
+
+            case "up":
+                currentPosition.setY(currentPosition.getY() - 1);
+                break;
+
+            case "down":
+                currentPosition.setY(currentPosition.getY() + 1);
+                break;
+        }
+    }
+
+    Position generateRandomExitPosition() {
+        int i = (int) ((Math.random() * (width - 1)));
+
+        return new Position(i, 0);
+    }
+
+    private Position generateRandomStartPosition() {
+        int i = (int) ((Math.random() * (width - 1)));
+
+        return new Position(i, height - 1);
+    }
+
+    public List<List<Room>> getGrid() {
+        return grid;
+    }
+}
---
+++ app/src/main/java/com/iarpg/app/ui/WelcomeFragment.java
@@ -0,0 +1,58 @@
+package com.iarpg.app.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iarpg.app.R;
+import com.iarpg.app.databinding.FragmentWelcomeBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WelcomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding  binding;
+    public static WelcomeFragment newInstance(String param1, String param2) {
+        WelcomeFragment fragment = new WelcomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                CharacterFragment characterFragment = CharacterFragment.newInstance();
+                fragmentTransaction.add(R.id.fragment_container_view, characterFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+    }
+}
