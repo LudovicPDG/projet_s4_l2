62f99611eb96b66ab5190d2baf19fd32b388ccb9
Bj-LAI <bjornlai13@gmail.com>
2025-04-09 20:03:25 +0200

    Ajustement des prompts et paramètres

--- src/main/java/com/iarpg/backend/backend/AIGenerator.java
+++ src/main/java/com/iarpg/backend/backend/AIGenerator.java
@@ -30,20 +30,23 @@ public class AIGenerator {
 
 
     @GetMapping("/generateRoomDescription")
-    public ResponseEntity<Map<String, Object>> generateRoomDescription(@RequestParam String characterClass ,@RequestParam String roomTitle, @RequestParam String previousRoomTitle, @RequestParam Boolean finalRoom) {
+    public ResponseEntity<Map<String, Object>> generateRoomDescription(@RequestParam String roomTitle, @RequestParam String previousRoomTitle, @RequestParam Boolean finalRoom, @RequestParam String characterClass, @RequestParam int playerHealthPoints, @RequestParam String theme) {
         // Construction dynamique du prompt avec les conditions isTrapped et finalRoom
         String prompt = "Remplis le fichier JSON suivant en remplaçant chaque '?' par une valeur selon les critères suivants : \n" +
+                "* L'ensemble de l'histoire doit respecter le thème " + theme + "\n." +
                 "* roomTitle est égal au paramètre " + roomTitle + "\n" +
-                "* roomDescription est une description immersive d'une salle de RPG solo dont le nom est " + roomTitle +". Adresse-toi directement au joueur dans cette description. La classe du joueur est " + characterClass + ".\n" +
-                "Cette description doit commencer par décrire une transition entre la salle précédente de nom " + previousRoomTitle + " et la nouvelle salle. " +
+                "* roomDescription est une description immersive d'une salle de RPG solo dont le nom est " + roomTitle +". Adresse-toi directement au joueur en le vouvoyant dans cette description. La classe du joueur est " + characterClass + ".\n" +
+                "Cette description doit commencer par décrire une transition entre la salle précédente de nom " + previousRoomTitle + " et la nouvelle salle. Si previousRoomTitle = None, cela signifie que c'est la première salle du jeu." +
                 "Dans cette description, décris un obstacle (monstre, piège, ou obstacle dangereux)\n" +
-                "* choice1 et choice2 sont deux actions possibles pour le joueur. Un seul de ces deux choix a une issue favorable. L'autre est un choix pénalisant, mais non mortel.\n" +
+                "* choice1 et choice2 sont deux actions possibles pour le joueur. Un seul de ces deux choix a une issue favorable. L'autre choix doit affecter l'état de santé du joueur.\n" +
                 "La consequence positive doit aussi décrire l'obtention d'un item, défini par les champs itemName et itemDecription" +
                 "* itemName décrit le nom de l'item " +
                 "* itemDescription est une description de l'item sur une ligne" +
                 "* correctChoice est le numéro du bon choix (1 ou 2)\n" +
-                "* consequence1 et consequence2 sont les descriptions des conséquences respectivement aux actions 1 et 2. Les deux conséquences doivent finir par une sortie du joueur de la salle.\n\n" +
-                "{\"roomTitle\": \"?\", \"roomDescription\": \"?\", \"choice1\": \"?\", \"choice2\": \"?\", \"correctChoice\": \"?\", \"consequence1\": \"?\", \"consequence2\": \"?\", \"itemName\" : \"?\", \"itemDescription\" : \"?\"}";
+                "* consequence1 et consequence2 sont les descriptions des conséquences respectivement aux actions 1 et 2. Les deux conséquences doivent finir par une sortie du joueur de la salle." +
+                "* nextHealthPoints est le nombre de points de vie restants au joueur après la conséquence négative. Si le joueur tombe à court de vie (c'est-à-dire nextHealthPoints égale 0) , tu dois décrire sa mort. Le joueur doit perdre moins de 50 points de vie" +
+                "Le joueur a actuellement " + playerHealthPoints + "points de vie.\n\n" +
+                "{\"roomTitle\": \"?\", \"roomDescription\": \"?\", \"choice1\": \"?\", \"choice2\": \"?\", \"correctChoice\": \"?\", \"consequence1\": \"?\", \"consequence2\": \"?\", \"itemName\" : \"?\", \"itemDescription\" : \"?\", \"nextHealthPoints\" : \"?\"}";
 
         // Vérification des paramètres spécifiques
         if (finalRoom) {
@@ -107,6 +110,7 @@ public class AIGenerator {
             responseMap.put("consequence2", json.path("consequence2").asText());
             responseMap.put("itemName", json.path("itemName").asText());
             responseMap.put("itemDescription", json.path("itemDescription").asText());
+            responseMap.put("nextHealthPoints", json.path("nextHealthPoints").asText());
 
             return new ResponseEntity<>(responseMap, HttpStatus.OK);
 
@@ -122,7 +126,7 @@ public class AIGenerator {
         String url = GEMINI_API_URL + "?key=" + API_KEY;
 
         // Prompt
-        String prompt = "Génère une liste de 30 titres immersifs pour des salles dans un jeu de rôle fantasy. " +
+        String prompt = "Génère une liste de 30 titres immersifs pour des salles dans un jeu de rôle. " +
                 "Chaque titre doit être court (3 à 5 mots), évocateur et unique. " +
                 "Formate-les sous forme d’un tableau JSON de chaînes de caractères." +
                 (theme != null && !theme.isEmpty() ? " Le thème général est : " + theme + "." : "");
@@ -165,9 +169,11 @@ public class AIGenerator {
 
 
     @GetMapping("/generateBackstory")
-    public ResponseEntity<String> generateBackstory(@RequestParam String description) {
+    public ResponseEntity<String> generateBackstory(@RequestParam String characterClass, @RequestParam String description, @RequestParam String theme) {
         RestTemplate restTemplate = new RestTemplate();
-        String prompt = "Génère une courte histoire immersive expliquant le passé et les origines d'un personnage de jeu de role basé sur cette description : " + description;
+        String prompt = "Génère une courte histoire immersive d'environ 10 lignes expliquant le passé et les origines d'un personnage de jeu de role basé sur cette description : " + description + ".\n" +
+                        "Le thème de l'histoire est " + theme + ".\n" +
+                        "Adresse-toi directement au joueur en le vouvoyant comme s'il était ce personnage. Ne finis pas par une question. Termine en disant qu'un long périple attend le joueur.";
 
         String requestBody = String.format("{\"contents\":[{\"parts\":[{\"text\":\"%s\"}]}]}", prompt);
         String url = GEMINI_API_URL + "?key=" + API_KEY;
